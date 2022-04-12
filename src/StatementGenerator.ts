889c54289c1e95180adf8a3c559bab312bafc233
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-12 15:55:50 +0530

    Extract Named Constant

--- src/StatementGenerator.ts
+++ src/StatementGenerator.ts
@@ -1,5 +1,7 @@
 export class StatementGenerator {
+    readonly FARE_PER_KM=10;
+    readonly FARE_PER_MINUTE=1;
   totalFare = (time: number, distance: number): number => {
-    return 1 * time + 10 * distance;
+    return this.FARE_PER_MINUTE * time + this.FARE_PER_KM * distance;
   };
 }
