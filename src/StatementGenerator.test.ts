96d4900f12b3296cf06eb885fdbc4f7b3e36ebf5
Sanjay Rawat <63999697+SanjayRawat63@users.noreply.github.com>
2022-04-12 15:53:23 +0530

    Calculate totalFare For Given Distance and Time

--- src/StatementGenerator.test.ts
+++ src/StatementGenerator.test.ts
@@ -10,4 +10,9 @@ describe("Statement Generator Test", () => {
     const totalFare = statementGenerator.totalFare(0, 1);
     expect(totalFare).toBe(10);
   });
+  it("shouldCalculateForAGivenTimeAndAGivenDistance", () => {
+    const statementGenerator = new StatementGenerator();
+    const totalFare = statementGenerator.totalFare(1, 1);
+    expect(totalFare).toBe(11);
+  });
 });
